86a5f4211fbf43b85021bc799e33a57db1631c4e
Mark Bellaire <mark.bellaire@gmail.com>
2014-09-12 16:30:08 -0400

    Closes #27 Archiving Sprints

--- client/views/sprints-list.js
+++ client/views/sprints-list.js
@@ -1,4 +1,11 @@
 Template.sprintsList.sprints = function () {
   var sprintSearchQuery = Session.get('sprintSearchQuery');
-  return Sprints.find({title: {$regex: sprintSearchQuery, $options : "i"}}, {sort: {createdAt:-1}});
+  return Sprints.find({title: {$regex: sprintSearchQuery, $options : "i"}, archived: false}, {sort: {createdAt:-1}});
 };
+
+Template.sprintsList.events({
+  'click input[name="archiveSprint"]': function(e) {
+    e.preventDefault();
+    Sprints.update({_id: this._id}, {$set: {archived: true}});
+  }
+});
