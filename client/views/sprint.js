07f86bfc81077fbd32c01fa8332bae32da90fd5e
Mark Shropshire <mdshrops@shropnet.net>
2014-09-12 16:33:03 -0400

    Fixed logo on sprint pages. Work on sprint page.

--- client/views/sprint.js
+++ client/views/sprint.js
@@ -3,3 +3,8 @@ Template.sprint.helpers({
     return Sprints.find({_id: this._id});
   }
 });
+
+Template.issuesList.issues = function () {
+  var sprintSearchQuery = Session.get('sprintSearchQuery');
+  return Sprints.find({title: {$regex: sprintSearchQuery, $options : "i"}}, {sort: {createdAt:-1}});
+};
