29dca0a9ca0bccab65254109c5ecbd1e56ef28e4
Mark Shropshire <mdshrops@shropnet.net>
2014-09-12 16:12:32 -0400

    Sprints display by descending order using createdAt as the sort field

--- client/views/sprints-list.js
+++ client/views/sprints-list.js
@@ -1,4 +1,4 @@
 Template.sprintsList.sprints = function () {
   var sprintSearchQuery = Session.get('sprintSearchQuery');
-  return Sprints.find({title: {$regex: sprintSearchQuery, $options : "i"}});
+  return Sprints.find({title: {$regex: sprintSearchQuery, $options : "i"}}, {sort: {createdAt:-1}});
 };
