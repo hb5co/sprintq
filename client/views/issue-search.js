ca707a6e107215c2a0fe634aec00a984df90d634
Mark Bellaire <mark.bellaire@gmail.com>
2014-09-13 17:30:43 -0400

    Closes #60 Add reactive search on Issues and Sprints

--- client/views/issue-search.js
+++ client/views/issue-search.js
@@ -3,4 +3,16 @@ Template.issueSearch.events({
     e.preventDefault();
     Session.set('issueSearchQuery', '.*' + $('input[name="issueSearch"]').val() + '.*');
   },
+  'keyup form': function(e) {
+    Session.set('issueSearchQuery', '.*' + $('input[name="issueSearch"]').val() + '.*');
+    if (e.which === 27) {
+      $('#issueSearchInput').val('');
+      Session.set('issueSearchQuery', '.*' + $('input[name="issueSearch"]').val() + '.*');
+    }
+  },
+  'blur form': function(e) {
+    e.preventDefault();
+    $('#issueSearchInput').val('');
+    Session.set('issueSearchQuery', '.*' + $('input[name="issueSearch"]').val() + '.*');
+  }
 });
