37974ee67b48242b54dbd357520b4b694ff12609
Mark Shropshire <mdshrops@shropnet.net>
2014-09-13 08:55:38 -0400

    sprintQ: Now with iternating issues

--- client/views/sprint.js
+++
@@ -1,9 +0,0 @@
-Template.sprint.helpers({
-  sprint: function() {
-    return Sprints.find({_id: this._id});
-  }
-});
-
-Template.sprint.issues = function () {
-  return Sprints.find({_id: {$in: issues}});
-};
