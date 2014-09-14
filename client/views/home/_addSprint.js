70e942140b808d570f6262cdc43441b6d13e295b
Mark Bellaire <mark.bellaire@gmail.com>
2014-09-14 14:48:12 -0400

    Closes #68 Cannot add sprint with empty title
    I am going to work on keeping a sprint from changing to an empty title.

--- client/views/home/_addSprint.js
+++ client/views/home/_addSprint.js
@@ -2,8 +2,10 @@ Template.addSprint.events({
   'submit form': function(e) {
     e.preventDefault();
     var newSprint = document.getElementById('sprint-title').value;
-    Sprints.insert({title: newSprint});
-    $('.sprints-list .sprint-row').eq(0).hide().addClass('highlight').slideDown(250);
-    document.getElementById('sprint-title').value = '';
+    if (newSprint !== ''){
+      Sprints.insert({title: newSprint});
+      $('.sprints-list .sprint-row').eq(0).hide().addClass('highlight').slideDown(250);
+      document.getElementById('sprint-title').value = '';
+    }
   }
 });
