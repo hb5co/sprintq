eaabd300519dc78f642cb81c704f100c94a8c19d
Mark Bellaire <mark.bellaire@gmail.com>
2014-09-14 10:02:09 -0400

    Fix id typo
    The id of the edit Sprint title field had a typo.

--- client/views/home/_editSprintTitle.js
+++ client/views/home/_editSprintTitle.js
@@ -2,7 +2,7 @@ Template.editSprintTitle.events({
   'submit #editSprintTitleForm': function(e) {
     e.preventDefault();
     var sprintId = Session.get('editingSprintTitleId');
-    var newTitle = $('#editSptintTitleInput').val();
+    var newTitle = $('#editSprintTitleInput').val();
     Sprints.update({_id: sprintId}, {$set: {title: newTitle}});
     Session.set('editingSprintTitleId', false);
   },
