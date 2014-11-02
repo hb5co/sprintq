ad8c5da76a3a468c115fe89d63c381b30d2b4636
Mark Bellaire <mark.bellaire@gmail.com>
2014-11-02 15:49:03 -0500

    Prompt user before archiving sprint
    I am using the alertify js library (installed via meteor package ovcharik:alertify) to show the alert and ensure that the user does, in fact, want to archive a sprint.

--- client/views/home/_sprintsList.js
+++ client/views/home/_sprintsList.js
@@ -26,15 +26,21 @@ Template.sprintsList.events({
     e.stopPropagation();
     e.preventDefault();
 
-    sprintId = this._id;
+    alertify.confirm("Are you sure you want to archive this sprint?", function (e1) {
+      if (e1) {
+        sprintId = this._id;
 
-    // getting clicked button and it's table row
-    var archiveSprint = $(e.currentTarget).parent().parent().parent().parent();
+        // getting clicked button and it's table row
+        var archiveSprint = $(e.currentTarget).parent().parent().parent().parent();
 
-    archiveSprint.removeClass('highlight').addClass('animated fadeOutLeftBig archived');
+        archiveSprint.removeClass('highlight').addClass('animated fadeOutLeftBig archived');
 
-    archiveSprint.delay(250).slideUp(500, function(){
-      Sprints.update({_id: sprintId}, {$set: {archived: true}});
+        archiveSprint.delay(250).slideUp(500, function(){
+          Sprints.update({_id: sprintId}, {$set: {archived: true}});
+        });
+      } else {
+        alertify.log("Canceled sprint archiving.");
+      }
     });
   },
   'click #editSprintTitleBtn': function(e) {
