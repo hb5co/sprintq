4fb4b42dda7f093677e6ee2170eccc58a15eec45
Nathan Costea <nathanC@knowclassic.com>
2014-11-10 08:24:28 -0500

    Fix the RegisterHelpers after the update to 1.0

--- client/helpers/routes.js
+++ client/helpers/routes.js
@@ -1,9 +1,9 @@
 // Helper for current route name.
 UI.registerHelper('currentRouteName',function() {
-  return Router.current() ? Router.current().route.name : '';
+  return Router.current() ? Router.current().route.getName() : '';
 });
 
 // Helper to help check the current route.
 UI.registerHelper('RouteIs', function (routeName) {
-  return Router.current().route.name === routeName;
+  return Router.current().route.getName() === routeName;
 });
